b6d61ee01593b157caa4148a26f4201d23242512
bangjunwoo <bang.jun@sidewalkplay.com>
2024-03-17 16:56:51 +0900

    실패한 코드2

--- src/Fn/main.ts
+++ src/Fn/main.ts
@@ -216,47 +216,61 @@ type Tes = GeneratorReturn<ReturnType<typeof combinationIter<Te>>>;
 
 function* combine<T extends Tes>(iter: IterableIterator<T>) {
   const skipType = [""];
-  const commponent = {} as { [key: string]: string[] };
+  // const commponent = {} as { [key: string]: string[] };
+  let commponent = [] as string[];
+  let prevCommponent = [] as string[];
   let code = "";
+  let minDepth = Infinity;
+  let count = 0;
   for (const value of iter) {
     const { prev, current } = value;
 
     // 확인하고 싶은 것 , 동기성을 유지하는지
     // 클로저를 유지하는지
-    if (typeof prev !== "string" && typeof current !== "string") {
+    if (typeof current !== "string") {
       // console.log("prev", textPostion(prev));
       // console.log("current", textPostion(current));
       // 2. 코드 추가 중에 닫히는 상황 발생 시 지금까지 작성한 코드를 감싸는 컴포넌트를 만든다
       // 인스턴스 객체일 경우는 이전 검색에서 pass 여부를 넣어줘야한다
       // 감싸진 객체는 특정 조건을 만족 할 때 까지 코드를 생성한다
       // 조건이 만족되면 code를 commponent 로 선언한다?
+      // 안쪽의 prev에도 pipe를 넣을 수 있지 않을까
 
-      const { len: prevLen, tagName: prevTagName } = textPostion(prev);
-      // 1. 여기서는 code를 추가한다
-      const { len: currentLen, tagName, id } = textPostion(current);
+      const { len, tagName, id } = textPostion(current);
+      console.log("diff::", "currentLen::", len);
 
-      const diff = prevLen - currentLen;
-
-      if (diff > 0) {
-        console.log("diff", diff, code);
-        code = `${tabText.repeat(currentLen)}<${tagName}@@@@${current.node.id}>
-${code}
-${tabText.repeat(currentLen)}</${tagName}@@@@${current.node.id}>
-`;
-        if (!commponent[currentLen]) commponent[currentLen] = [];
-        commponent[currentLen].push(code);
-        code = "";
-      } else {
-        code += `${tabText.repeat(currentLen)}<${tagName}@@@@${current.node.id}>
-${commponent[currentLen + 1]?.join("")}
-${tabText.repeat(currentLen)}</${tagName}@@@@${current.node.id}>
-`;
-        commponent[currentLen + 1] = [];
+      /**
+       * 1이면 이전 값들을 가져와야한다
+       * 0이면 code를 code에 추가해야한다
+       * -1 ~ 일 경우에는 code를 초기화한다
+       * @returns prev - current
+       */
+      const isPrev = (prev2: Ast1 | "start" | "end", currentLen: number) => {
+        if (typeof prev2 === "string") return -1;
+        const { len: prevLen } = textPostion(prev2);
+        minDepth = Math.min(minDepth, prevLen);
+        return prevLen - currentLen;
+      };
+      const expr = isPrev(prev, len);
+      if (expr === 1) {
+        // 현재 탐색 중인 높이가 현재까지 탐색된 최상단 노드와 같다면
+        if (minDepth === len) {
+        }
       }
-    } else if (prev === "start") {
-      const { len: currentLen, tagName } = textPostion(current as Ast1);
-      code += `${tabText.repeat(currentLen)}<${tagName}/${(current as Ast1).node.id}>
+      if (expr < 0) {
+        // 마이너스 일 경우, 이전 값은 탐색을 끝냈다는 뜻 ,
+        // -1 이면 형제 탐색중
+        // -n 이면 다음 형제의 첫 노드가 아주 깊게 있다는 뜻
+        // 한 컴포넌트 내부 순회를 끝내고 다음, 또는 start
+        code = "";
+      }
+      if (expr === 0) {
+        code = `${tabText.repeat(len)}<${tagName}/${current.node.id}>
+${count++}
+${tabText.repeat(len)}</${tagName}@@@@${current.node.id}>
 `;
+      }
+      console.log(expr);
     }
   }
   yield commponent;
@@ -279,8 +293,8 @@ const ast = async (node: BaseNode) => {
   const semantic = pipe(unPack, prevIter, combinationIter, combine, head);
 
   if (semantic) {
-    console.log(semantic);
-    Object.keys(semantic).forEach((e) => console.log(semantic[e]));
+    console.log(semantic.join(""));
+    // Object.keys(semantic).forEach((e) => console.log(semantic[e]));
   }
 
   // const semantic2 = pipe(
